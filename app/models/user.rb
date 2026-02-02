8a6bcce930c8274b13bfe153cad6838af6a406d8
Azuna <36605286+azunaVT@users.noreply.github.com>
2026-02-01 23:39:09 -0500

    Added Register endpoints, tests and changed validation around user/sessions to match the test expectations when necessary (#47)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates :email, presence: true, uniqueness: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+  validates :password, presence: { on: :create }, length: { minimum: 8 }, if: -> { password.present? }
 end
